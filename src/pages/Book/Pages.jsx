3c54658d1186e4cc91e8f42f529dce850c491dd9
affan <affan.habib44@gmail.com>
2024-08-08 16:12:24 +0600

    updated

--- src/pages/Book/Pages.jsx
+++ src/pages/Book/Pages.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import HTMLFlipBook from 'react-pageflip';
 
-const Pages = () => {
+const Book = () => {
   return (
-    <div>Pages</div>
-  )
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <HTMLFlipBook width={595} height={842} drawShadow={false} >
+        <div className="p-4 bg-gray-200 border">Page 1</div>
+        <div className="p-4 bg-gray-300 border">Page 2</div>
+        <div className="p-4 bg-gray-400 border">Page 3</div>
+        <div className="p-4 bg-gray-500 border">Page 4</div>
+      </HTMLFlipBook>
+    </div>
+  );
 }
 
-export default Pages
+export default Book;
